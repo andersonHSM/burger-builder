3743569383e95dcb9f75fd22e22b4e1e0a722b94
Anderson Menezes <andersonhsantanappa@gmail.com>
2020-06-19 08:29:01 -0300

    structural changes

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,9 @@
 import { createStore } from 'redux';
 import ingredientsReducer from './reducers/ingredients.reducers';
 
-const store = createStore(ingredientsReducer);
+const store = createStore(
+  ingredientsReducer,
+  (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store;
